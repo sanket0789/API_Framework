5948d2fb2aed3e1895316534ff777828f177dcb0
sanket0789 <sanket0789@users.noreply.github.com>
2021-01-23 12:13:13 -0500

    Personal_machine_Commit

--- src/test/java/API/Get_Payload_From_External_File.java
+++ src/test/java/API/Get_Payload_From_External_File.java
@@ -12,26 +12,27 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 
 public class Get_Payload_From_External_File {
-	
+
 	@Test
-	public void Add_Place() throws IOException 
-	{
-	RestAssured.baseURI="https://rahulshettyacademy.com";
-	
-	//When getting Json from file, First step is to convert Json Into String.
-	
-			//Given - all input details
-			String Response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-			.body(new String(java.nio.file.Files.readAllBytes(Paths.get("C:\\Users\\Kumar Joshi\\Desktop\\Add_Place.json"))))
-			//When - Submit API
-			.when().post("/maps/api/place/add/json")
+	public void Add_Place() throws IOException {
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
 
-			//Then - Validate the response.
-			.then().assertThat().statusCode(200).body("scope", equalTo("APP"))
-			.header("Server", "Apache/2.4.18 (Ubuntu)").extract().response().asString();
+		// When getting Json from file, First step is to convert Json Into String.
 
+		// Given - all input details
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
 
-	System.out.println("===================================================================");
+				// Provide path of the file in body to get Json from the file.
+				.body(new String(java.nio.file.Files.readAllBytes(Paths.get("C:\\Users\\Sanket\\Desktop\\Add_Place.json"))))
+
+				// When - Submit API
+				.when().post("/maps/api/place/add/json")
+
+				// Then - Validate the response.
+				.then().assertThat().statusCode(200).body("scope", equalTo("APP"))
+				.header("Server", "Apache/2.4.18 (Ubuntu)").extract().response().asString();
+
+		System.out.println("===================================================================");
 	}
 
 }
